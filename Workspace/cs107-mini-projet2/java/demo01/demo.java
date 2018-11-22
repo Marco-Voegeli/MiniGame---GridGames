6c0590813016feab6570f618d619d70ca39b94d0
Roy shoham <roy.shoham95@gmail.com>
2018-11-22 10:54:52 +0100

    Demo

---
+++ Workspace/cs107-mini-projet2/java/demo01/demo.java
@@ -0,0 +1,44 @@
+package demo01;
+
+import ch.epfl.cs107.play.game.Game;
+import ch.epfl.cs107.play.io.FileSystem;
+import ch.epfl.cs107.play.window.Window;
+
+public class demo implements Game{	
+
+	@Override
+	public boolean begin(Window window, FileSystem fileSystem) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return "Demo1";
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getFrameRate() {
+		// TODO Auto-generated method stub
+		return 24;
+	}
+
+	
+	
+	
+	
+	
+}
