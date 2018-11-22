3d7d3a533865ef470ceac7485fa3401d0ac75a7e
Marco Voegeli <alvin.mvoegeli@gmail.com>
2018-11-22 13:57:04 +0100

    Fixed github merge bug

---
+++ Workspace/cs107-mini-projet2/java/demo1/actor/MovingRock.java
@@ -0,0 +1,25 @@
+package demo1.actor;
+
+import java.awt.Color;
+
+import ch.epfl.cs107.play.game.actor.GraphicsEntity;
+import ch.epfl.cs107.play.game.actor.ImageGraphics;
+import ch.epfl.cs107.play.game.actor.TextGraphics;
+import ch.epfl.cs107.play.game.areagame.io.ResourcePath;
+import ch.epfl.cs107.play.math.Vector;
+
+public class MovingRock extends GraphicsEntity {
+	protected final TextGraphics text;
+
+	public MovingRock(Vector position, String text) {
+		super(position, new ImageGraphics(ResourcePath.getSprite("rock.3.png"), 0.1f, 0.1f, null, Vector.ZERO, 1.0f,
+				-Float.MAX_VALUE));
+		this.text = new TextGraphics(text,0.04f,Color.BLUE);
+		this.text.setParent(this);
+		this.text.setAnchor(new Vector(-0.3f,0.1f));
+	}
+	public TextGraphics getText() {
+		return text;
+	}
+
+}
