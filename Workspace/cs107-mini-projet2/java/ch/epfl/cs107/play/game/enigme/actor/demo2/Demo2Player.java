35b5beeff2d42c7651f949a090a2865092243f09
Marco Voegeli <alvin.mvoegeli@gmail.com>
2018-12-02 17:48:02 +0100

    door

--- Workspace/cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/demo2/Demo2Player.java
+++ Workspace/cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/demo2/Demo2Player.java
@@ -4,10 +4,13 @@ import java.util.Collections;
 import java.util.List;
 
 import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.AreaBehavior.Cell;
 import ch.epfl.cs107.play.game.areagame.actor.MovableAreaEntity;
 import ch.epfl.cs107.play.game.areagame.actor.Orientation;
 import ch.epfl.cs107.play.game.areagame.actor.Sprite;
+import ch.epfl.cs107.play.game.enigme.Demo2Behavior.Demo2CellType;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import ch.epfl.cs107.play.window.Button;
 import ch.epfl.cs107.play.window.Canvas;
 import ch.epfl.cs107.play.window.Keyboard;
 import ch.epfl.cs107.play.window.Window;
@@ -16,12 +19,14 @@ public class Demo2Player extends MovableAreaEntity {
 
 	private boolean passingdoor = false;
 	private Window window;
+	private Sprite demoSprite;
 	/// Animation duration in frame number
 	private final static int ANIMATION_DURATION = 8;
 
 	public Demo2Player(Area area, Orientation orientation, DiscreteCoordinates position) {
 		super(area, orientation, position);
-		new Sprite("ghost.1", 1, 1.f, this);
+		demoSprite = new Sprite("ghost.1", 1, 1.f, this);
+		this.setOrientation(Orientation.DOWN);
 	}
 
 	@Override
@@ -49,17 +54,66 @@ public class Demo2Player extends MovableAreaEntity {
 	@Override
 	public void draw(Canvas canvas) {
 		// TODO Auto-generated method stub
-
+		demoSprite.draw(canvas);
 	}
-
+	 @Override
+	protected boolean move(int framesForMove) {
+		// TODO Auto-generated method stub
+		 this.getOwnerArea().getEnteringCells()
+		 if()
+		return super.move(framesForMove);
+	}
+	 
+	
 	@Override
 	public void update(float deltaTime) {
 		super.update(deltaTime);
 		Keyboard keyboard = window.getKeyboard();
-		if (keyboard.get(Keyboard.LEFT).isDown()) {
+		Button[] Arrows = { keyboard.get(Keyboard.UP), keyboard.get(Keyboard.DOWN), keyboard.get(Keyboard.LEFT),
+				keyboard.get(Keyboard.RIGHT) };
+
+		if (Arrows[0].isDown()) {
+			up();
+
+		} else if (Arrows[1].isDown()) {
+			down();
+		} else if (Arrows[2].isDown()) {
+			left();
+		} else if (Arrows[3].isDown()) {
+			right();
+		}
+
+	}
+
+	public void up() {
+		if (this.getOrientation() == Orientation.UP) {
 			this.move(ANIMATION_DURATION);
 		} else {
-			this.setOrientation(this.getOrientation().hisLeft());
+			this.setOrientation(Orientation.UP);
+		}
+	}
+
+	public void down() {
+		if (this.getOrientation() == Orientation.DOWN) {
+			this.move(ANIMATION_DURATION);
+		} else {
+			this.setOrientation(Orientation.DOWN);
+		}
+	}
+
+	public void left() {
+		if (this.getOrientation() == Orientation.LEFT) {
+			this.move(ANIMATION_DURATION);
+		} else {
+			this.setOrientation(Orientation.LEFT);
+		}
+	}
+
+	public void right() {
+		if (this.getOrientation() == Orientation.RIGHT) {
+			this.move(ANIMATION_DURATION);
+		} else {
+			this.setOrientation(Orientation.RIGHT);
 		}
 	}
 
