fab5deeb874c2944fb235774bfa8db44748b724d
Marco Voegeli <alvin.mvoegeli@gmail.com>
2018-12-02 17:05:35 +0100

    Demo2Player

--- Workspace/cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/demo2/Demo2Player.java
+++ Workspace/cs107-mini-projet2/java/ch/epfl/cs107/play/game/enigme/actor/demo2/Demo2Player.java
@@ -1,26 +1,33 @@
 package ch.epfl.cs107.play.game.enigme.actor.demo2;
 
+import java.util.Collections;
 import java.util.List;
 
 import ch.epfl.cs107.play.game.areagame.Area;
 import ch.epfl.cs107.play.game.areagame.actor.MovableAreaEntity;
 import ch.epfl.cs107.play.game.areagame.actor.Orientation;
+import ch.epfl.cs107.play.game.areagame.actor.Sprite;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.window.Canvas;
+import ch.epfl.cs107.play.window.Keyboard;
+import ch.epfl.cs107.play.window.Window;
 
-public class Demo2Player extends MovableAreaEntity  {
+public class Demo2Player extends MovableAreaEntity {
 
 	private boolean passingdoor = false;
+	private Window window;
+	/// Animation duration in frame number
+	private final static int ANIMATION_DURATION = 8;
 
 	public Demo2Player(Area area, Orientation orientation, DiscreteCoordinates position) {
 		super(area, orientation, position);
-		// TODO Auto-generated constructor stub
+		new Sprite("ghost.1", 1, 1.f, this);
 	}
 
 	@Override
 	public List<DiscreteCoordinates> getCurrentCells() {
 
-		return null;
+		return Collections.singletonList(getCurrentMainCellCoordinates());
 	}
 
 	@Override
@@ -44,9 +51,22 @@ public class Demo2Player extends MovableAreaEntity  {
 		// TODO Auto-generated method stub
 
 	}
+
+	@Override
+	public void update(float deltaTime) {
+		super.update(deltaTime);
+		Keyboard keyboard = window.getKeyboard();
+		if (keyboard.get(Keyboard.LEFT).isDown()) {
+			this.move(ANIMATION_DURATION);
+		} else {
+			this.setOrientation(this.getOrientation().hisLeft());
+		}
+	}
+
 	public void enterArea(Area area, DiscreteCoordinates position) {
 		area.registerActor(this);
 		this.update(24f);
+		this.resetMotion();
 	}
 
 }
